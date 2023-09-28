bc0146549974991ee3cb9228613aab7c844f1982
thiagocsr <76794619+thiagocsr@users.noreply.github.com>
2023-09-28 17:16:54 -0300

    Finaliza classe reserva.

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,58 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            bool CapacidadeSuficiente = Suite.Capacidade >= hospedes.Count;
+            if (CapacidadeSuficiente)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("O número de hospedes é maior que a capacidade da suite.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            Console.WriteLine($"O valor da diária é: {Suite.ValorDiaria}");
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9M;
+            }
+
+            return valor;
+        }
+    }
+}
